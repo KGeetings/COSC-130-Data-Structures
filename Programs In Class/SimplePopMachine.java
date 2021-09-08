2d1e90881cd001276ee690d90ec8e0db7507c41a
Kenyon <kengeet56@gmail.com>
2021-09-07 21:55:28 -0500

    simplePopMachine update

--- Programs In Class/SimplePopMachine.java
+++ Programs In Class/SimplePopMachine.java
@@ -13,21 +13,38 @@ public class SimplePopMachine {
     //methods
     public void deposit(int amount){
         amountDeposited += amount;
+        System.out.println("Thanks for depositing! Balance: " + amountDeposited + "\u00A2.");
+    }
+    public void refund() {
+        System.out.println("Your current balance is: " + amountDeposited + "\u00A2.");
+        if(amountDeposited > 0){
+        amountDeposited = 0;
+        System.out.println("Your money has been refunded.");
+        }
     }
 
     public void select(){
         if(amountDeposited >= price) {
             System.out.println("Here's your: " + drink);
+            amountDeposited -= price;
+            refund();
         } else {
-        System.out.println("Please enter more money, you need " + (price-amountDeposited) + "\u00A2 more!");
+            refund();
         }
     }
 
    public static void main(String[] args) {
         SimplePopMachine machine = new SimplePopMachine("water", 100);
+        //money will be given back to user as there will not be enough in balance
         machine.deposit(50);
-        //machine.deposit(25);
-        machine.deposit(25);
         machine.select();
+        
+        //user will now deposit enough for one drink and select, leaving 0 balance left
+        machine.deposit(100);
+        machine.select();
+
+        //user will now deposit an amount and refund the money
+        machine.deposit(50);
+        machine.refund();
    } 
 }
