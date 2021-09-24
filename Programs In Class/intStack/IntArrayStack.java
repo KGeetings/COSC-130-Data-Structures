dd7b06d39e1b2ec7352d4559a8d0c18c53c5aac3
Kenyon <kengeet56@gmail.com>
2021-09-24 10:50:57 -0500

    intArrayStack added

--- Programs In Class/intStack/IntArrayStack.java
+++ Programs In Class/intStack/IntArrayStack.java
@@ -1,28 +1,69 @@
 package intStack;
-
+import java.util.EmptyStackException;
 public class IntArrayStack implements IntStack{
-    public void push(int value) {
+    //complications:
+    //pop empty
+    //peek
+    //full
 
+    private int top = -1;
+    private int[] data;
+    private static final int DEFAULT_CAPACITY = 10;
+
+    public IntArrayStack() {
+        data = new int[DEFAULT_CAPACITY];
+    }
+
+    public void push(int value) {
+        if (size() == data.length) {
+            resize(2 * data.length);
+        }
+        data[++top] = value;
     }
     public int pop() {
-        return 0;
+        if (isEmpty()) {
+            throw new EmptyStackException();
+        }
+        return data[top--];
     }
     public int size() {
-        return 0;
+        return (top + 1);
     }
     public int peek() {
-        return 0;
+        if (isEmpty()) {
+            throw new EmptyStackException();
+        }
+        return data[top];
     }
     public boolean isEmpty() {
-        return false;
+        return size() == 0;
+    }
+
+    private void resize(int newCapacity) {
+        System.out.println("Resizing to " + newCapacity);
+        int[] newData = new int[newCapacity];
+        // copy elements
+        for (int i = 0; i < data.length; i++) {
+            newData[i] = data[i];
+        }
+        data = newData;
     }
 
     public static void main(String [] args) {
         IntStack s = new IntArrayStack();
+        //s.pop();
         s.push(12);
         s.push(7);
+        System.out.println("Peek: " + s.peek());
         s.push(92);
+        s.push(35);
         System.out.println("Size: " + s.size());
         System.out.println("First pop: " + s.pop());
+        for (int i = 0; i <100; i++) {
+            s.push(i);
+        }
+        while(!s.isEmpty()) {
+            System.out.println(s.pop());
+        }
     }
 }
