3632fc4c912a5369dd25788662e8bc48a45fc753
Kenyon <kengeet56@gmail.com>
2021-10-11 13:39:27 -0500

    updated formatting of LinkedStack

--- Programs In Class/Stack/LinkedStack.java
+++ Programs In Class/Stack/LinkedStack.java
@@ -3,15 +3,17 @@ package Stack;
 import java.util.EmptyStackException;
 
 public class LinkedStack<E> implements Stack<E> {
-    private Node<E> top;
-    private int size;
+   private Node<E> top;
+   private int size;
 
-    public void push(E item) {
+   @SuppressWarnings("unchecked")
+   public void push(E item) {
       top = new Node(item, top);
       size++;
-    }
+   }
 
-    public E pop() {
+   @SuppressWarnings("unchecked")
+   public E pop() {
       if (isEmpty()) {
          throw new EmptyStackException();
       }
@@ -19,48 +21,48 @@ public class LinkedStack<E> implements Stack<E> {
       top = top.next;
       size--;
       return result;
-    }
+   }
 
-    public boolean isEmpty() {
-       return size() == 0;
-    }
+   public boolean isEmpty() {
+      return size() == 0;
+   }
 
-    public int size() {
-       return size;
-    }
+   public int size() {
+      return size;
+   }
 
-    public E peek() {
-       if (isEmpty()) {
-          throw new EmptyStackException();
-       }
-       return top.data;
-    }
+   public E peek() {
+      if (isEmpty()) {
+         throw new EmptyStackException();
+      }
+      return top.data;
+   }
 
-    private static class Node<T> {
-       private T data;
-       private Node next;
+   private static class Node<T> {
+      private T data;
+      private Node next;
 
-       private Node(T data, Node next) {
-          this.data = data;
-          this.next = next;
-       }
-    }
+      private Node(T data, Node next) {
+         this.data = data;
+         this.next = next;
+      }
+   }
     
-    public static void main(String[] args) {
-       Stack<String> words = new LinkedStack<>();
-       String[] examples = {"antelope", "pumpkin", "chocolate", "silence"}; 
-       
-       for (String word: examples) {
-          words.push(word);
-       }
+   public static void main(String[] args) {
+      Stack<String> words = new LinkedStack<>();
+      String[] examples = {"antelope", "pumpkin", "chocolate", "silence"}; 
+      
+      for (String word: examples) {
+         words.push(word);
+      }
 
-       Stack<Integer> numbers = new LinkedStack<>();
-       for (int i = 10; i < 20; i++) {
-          numbers.push(i*i);
-       }
-       
-       while (!numbers.isEmpty()) {
-          System.out.println(numbers.pop());
-       }
-    }
- }
+      Stack<Integer> numbers = new LinkedStack<>();
+      for (int i = 10; i < 20; i++) {
+         numbers.push(i*i);
+      }
+
+      while (!numbers.isEmpty()) {
+         System.out.println(numbers.pop());
+      }
+   }
+}
