bcda2fa86dd0c2049b8fa561eadede0a50c97722
Kenyon <kengeet56@gmail.com>
2021-10-11 10:50:01 -0500

    added expression.java

---
+++ Programs In Class/Stack/Expression.java
@@ -0,0 +1,29 @@
+package Stack;
+
+public class Expression {
+    private static final String SPACE = " ";
+
+    public static String toPostfix(String expr) {
+        StringBuilder result = new StringBuilder();
+        Stack<String> operators = new ArrayStack<>();
+        for (String token : expr.split(SPACE)) {
+            if (isOperator(token)) {
+                //pop equal or higher
+                operators.push(token);
+            } else {
+                result.append(token + SPACE);
+            }
+        }
+        //add to result by calling result.append(WHATEVER)
+        return result.toString();
+    }
+
+    private static boolean isOperator(String token) {
+        return false;
+    }
+
+    public static void main(String[] args) {
+        String expr = "1 + 2";
+        System.out.println("Postfix: " + toPostfix(expr));
+    }
+}
