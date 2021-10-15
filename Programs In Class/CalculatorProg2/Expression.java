50bd295336ca91d798d3ca9bd6191eddd2a38a1a
Kenyon <kengeet56@gmail.com>
2021-10-15 13:32:26 -0500

    added description of assignment

--- Programs In Class/CalculatorProg2/Expression.java
+++ Programs In Class/CalculatorProg2/Expression.java
@@ -1,5 +1,17 @@
 package CalculatorProg2;
 
+/* 
+Finish the applyOperator() and evalPostfix() functions in the Expression class to make the calculator work:
+   applyOperator() is described in exercise #7 in 4.4
+   evalPostfix() is exercise #8.  Follow Listing 4.4 on page 77.  Use a stack of integers, and notice the top of page 83: 
+   you will need to call Integer.parseInt(token) to convert a string token to an int.
+
+Start from the attached code; you should not need to make any changes to the Calculator class.
+Extra credit: make the decimal point key work and change all computations to use doubles instead of ints.  
+You will need to make two small changes in the Calculator class (one of them is marked), 
+and most of the other changes will be in the Expression class.  If you try this, do not make Expression generic; 
+just change it to use doubles.  Extra credit will only be given if the main part of the assignment is essentially correct.
+ */
 public class Expression {
    public static final String SPACE = " ";
    public static final String PLUS = "+";
