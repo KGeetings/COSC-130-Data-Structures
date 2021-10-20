97a68d8cf6e836792d7b7fbbb4ca6f6a404c65b8
Kenyon <kengeet56@gmail.com>
2021-10-20 10:49:41 -0500

    enqueue in arrayqeueu

--- Programs In Class/Queue/ArrayQueue.java
+++ Programs In Class/Queue/ArrayQueue.java
@@ -1,22 +1,65 @@
 package Queue;
 
+import java.util.NoSuchElementException;
+
 public class ArrayQueue<E> implements Queue<E>{
+    private Object[] data;
+    private int size;
+    private int capacity;
+    private int front;
+    private int rear = -1;
+    private static final int DEFAULT_CAPACITY = 10;
+
+    public ArrayQueue(int capacity) {
+        data = new Object[capacity];
+        this.capacity = capacity;
+    }
+
+    public ArrayQueue() {
+        this(DEFAULT_CAPACITY);
+    }
+
     public void enqueue(E item) {
-       
+        if (capacity == rear) {
+            rear = (rear + 1) % capacity;
+            return;
+        }
+        else {
+            data[rear] = data;
+            rear++;
+        }
+        return;
     }
 
     public E dequeue() {
+        /* if (isEmpty()) {
+            throw new NoSuchElementException();
+        } */
         return null;
     }
 
     public E peek() {
+        if (isEmpty()) {
+            throw new NoSuchElementException();
+        }
         return null;
     }
 
     public int size() {
-        return 0;
+        return size;
     }
+
     public boolean isEmpty() {
-        return false;
+        return size == 0;
     }
+
+    public static void main(String[] args) {
+        Queue<Integer> q = new ArrayQueue<>();
+        q.enqueue(2);
+        q.enqueue(12);
+        q.enqueue(6);
+        q.enqueue(27);
+        System.out.println("Dequeue " + q.dequeue());
+        System.out.println("Size " + q.size());
+     } 
 }
