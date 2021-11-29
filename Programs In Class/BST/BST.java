edf9175ee2e427766ab7e151e5312d5315d683ae
Kenyon <kengeet56@gmail.com>
2021-11-29 10:50:11 -0600

    updated BST

--- Programs In Class/BST/BST.java
+++ Programs In Class/BST/BST.java
@@ -92,18 +92,55 @@ public class BST {
         }
     }
 
-    /* private Node predNode(Node n) {
-        Node n = findNode(data, root);
-    }
-
     public int pred(int data){
         Node n = findNode(data, root);
         if (n == null) {
             throw new NoSuchElementException();
-        } else {
-            return predNode(n).data;
         }
-    } */
+        Node pred = predNode(n);
+        if (pred == null) {
+            throw new NoSuchElementException();
+        }
+        return pred.data;
+    }
+
+    private Node predNode(Node n) {
+        assert n != null;
+        if (n.left != null) { // if n has a left child, return the max node of n's left child
+            return maxNode(n.left);
+        } 
+        // move up until there's a right child
+        Node p = n.parent;
+        while (p != null && n == p.left) {
+            n = p;
+            p = p.parent;
+        }
+        return p;
+    }
+
+    public void remove(int data) {
+        Node n = findNode(data, root);
+        if (n != null) {
+            removeNode(n);
+        }
+    }
+
+    private void removeNode(Node n) {
+        assert n != null;
+        if (n.left == null) {
+            replace(n, n.right);
+        } else if (n.right == null) {
+            replace(n, n.left);
+        } else { // 2 children
+            Node pred = predNode(n);
+            n.data = pred.data;
+            removeNode(pred);
+        }
+    }
+
+    private void replace(Node n, Node child) {
+        // replace n with its child in the tree
+    }
 
     @Override
     public String toString() {
@@ -120,7 +157,7 @@ public class BST {
 
     public static void main(String[] args) {
         BST t = new BST();
-        int[] nums = {17, 5, 92, 50, 8, 23, 14, 37, 75};
+        int[] nums = {17, 5, 92, 50, 8, 23, 14, 37, 75, 9, 13};
         for (int num: nums) {
             t.add(num);
         }
@@ -130,5 +167,11 @@ public class BST {
 
         System.out.println("Max: " + t.max());
         System.out.println("Min: " + t.min());
+
+        System.out.println("Pred of 37: " + t.pred(37));
+        System.out.println("Pred of 17: " + t.pred(17));
+        System.out.println("Pred of 23: " + t.pred(23));
+        System.out.println("Pred of 14: " + t.pred(14));
+        // System.out.println("Pred of 5: " + t.pred(5));
     }
 }
